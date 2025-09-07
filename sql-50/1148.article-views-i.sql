2f316d32d317bce4fedc1f66ea2054238b728c40
Nhat Anh <nhatanhtruong20@gmail.com>
2025-09-07 15:54:57 +0700

    done problems 1148, 584, and 595

---
+++ sql-50/1148.article-views-i.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode id=1148 lang=mysql
+--
+-- [1148] Article Views I
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT distinct author_id as id FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC
+-- @lc code=end
+

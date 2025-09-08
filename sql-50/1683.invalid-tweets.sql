149257edb5a107b2bc5e1aefa7c832ec7664b528
Nhat Anh <nhatanhtruong20@gmail.com>
2025-09-08 19:03:11 +0700

    done problems 1378 and 1683

---
+++ sql-50/1683.invalid-tweets.sql
@@ -0,0 +1,12 @@
+--
+-- @lc app=leetcode id=1683 lang=mysql
+--
+-- [1683] Invalid Tweets
+--
+
+-- @lc code=start
+-- Write your PostgreSQL query statement below
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content) > 15
+-- @lc code=end
+
